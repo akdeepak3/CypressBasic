d04133cbce75cb2cf274c3245b8dc63abf93ce47
Deepak A K <akdeepak3@gmail.com>
2022-06-21 16:00:04 +0530

    Added JS file for Create Result txt File

---
+++ WriteFile.js
@@ -0,0 +1,27 @@
+const fs = require("fs");
+
+const readFilePath = "C:/Users/akdee/Desktop/TestingLearn/Cypress Basic-MyOwn/CypressBasic/Newt-scripts-test-results.txt"
+const writeFilePath = "C:/Users/akdee/Desktop/TestingLearn/Cypress Basic-MyOwn/CypressBasic/test-results.txt"
+const customreadFilePath = "C:/Users/akdee/Desktop/TestingLearn/Cypress Basic-MyOwn/CypressBasic/Mail footer.txt"
+
+var fileString = fs.readFileSync(
+    readFilePath,
+    { encoding: "utf8" }
+);
+
+var tableContent = fileString.split(" (Run Finished)")[1];
+
+var customfileString = fs.readFileSync(
+    customreadFilePath,
+    { encoding: "utf8" }
+);
+
+tableContent = "Please find the Overview Report" + tableContent + customfileString;
+
+fs.writeFile(writeFilePath, tableContent,
+    (err) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+    })
